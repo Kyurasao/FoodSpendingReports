6d3c42c29d1392788dbea845e32853706e5c5f79
Kyurasao <123263834+Kyurasao@users.noreply.github.com>
2023-07-18 13:04:44 +0400

    fix #2 | added the discription in main.py

---
+++ main.py
@@ -0,0 +1,5 @@
+"""
+This is a program for collecting the amount of food eaten and issuing reports.
+Users will be asked for information about their daily diet, and
+a report on the amount of food eaten per day will be issued.
+"""

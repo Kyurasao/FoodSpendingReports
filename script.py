c164475941ca84749c78b4014509554f59944507
Kyurasao <123263834+Kyurasao@users.noreply.github.com>
2023-08-11 00:40:26 +0400

    #13 | created script 'script.py' for working with sqlite3 and his db 'example_db.db'

--- script.py
+++ script.py
@@ -3,73 +3,30 @@ import time
 
 
 def main():
-    # conn = sqlite3.connect('example.db')
-    # cursor = conn.cursor()  # зачем эта строка??
-    # # time TEXT, time: Union[str, datetime]
-    # ... if cursor.execute else cursor.execute('''CREATE TABLE example
-    #                (id INTEGER PRIMARY KEY,
-    #                 name TEXT,
-    #                 time TEXT)''')
-    # conn.close()
-
-    # # Проверка наличия базы данных
-    # global conn
-    # try:
-    #     conn = sqlite3.connect('example.db')
-    # except sqlite3.Error as e:
-    #     if e.args[0] == 'table mytable does not exist':
-    #         # Создание базы данных и таблиц
-    #         conn = sqlite3.connect('example.db')
-    #         c = conn.cursor()
-    #         c.execute('''CREATE TABLE example
-    #                   (id INTEGER PRIMARY KEY,
-    #                    name TEXT,
-    #                    time TEXT)''')
-    #         conn.commit()  # зачем эта строка??
-    #         print('База данных создана')
-    #     else:
-    #         print(f'Ошибка при создании базы данных: {e}')
-    # finally:
-    #     # Закрытие соединения
-    #     conn.close()
-
-    global con
-    try:
-        con = sqlite3.connect('example.db')
-        cur = con.cursor()
-        cur.execute('''CREATE TABLE example
-                       (id INTEGER PRIMARY KEY,
-                        name TEXT,
-                        time TEXT)''')
-        rows = cur.fetchall()
-        if len(rows) > 0:
-            print('Table exists')
-        else:
-            # cur.execute(f"CREATE TABLE mytable(name text, time text)")
-            cur.execute('''CREATE TABLE example
-                                   (id INTEGER PRIMARY KEY,
-                                    name TEXT,
-                                    time TEXT)''')
-            con.commit()
-            print('Table created')
-
-    except sqlite3.OperationalError:
-        print('Database doesn’t exist')
-    finally:
-        con.close()
+    con = sqlite3.connect('example_db.db')
+    cur = con.cursor()
+    res = cur.execute("""SELECT name FROM sqlite_master WHERE name = 'example_table'""").fetchone()
+    if not res:
+        print("Table 'example_table' not found!")
+        cur.execute("""CREATE TABLE example_table (id INTEGER PRIMARY KEY, name TEXT, time TEXT)""")
+        print("Table 'example_table' created")
+    else:
+        print("Table 'example_table' found!")
+    # con.commit()
+    # con.close()
 
     while True:
         value = input("type: 'exit' to exit | 'create' to create a new entry \n")
         if value == 'exit':
             break
         elif value == 'create':
-            conn = sqlite3.connect('example.db')
-            cursor = conn.cursor()  # зачем эта строка??
+            # con = sqlite3.connect('example_db.db')
+            # cur = con.cursor()
             date_time = time.strftime('%d.%m.%Y %H:%M:%S', time.localtime(time.time()))
             name = input('Enter the name: ')
-            cursor.execute('INSERT INTO example (name, time) VALUES (?, ?)', (name, date_time))
-            conn.commit()
-            conn.close()
+            cur.execute('INSERT INTO example_table (name, time) VALUES (?, ?)', (name, date_time))
+            con.commit()
+    con.close()
 
 
 if __name__ == '__main__':

27c69e29d73fe59f7ea885011f4b3e819fa02dfa
Kyurasao <123263834+Kyurasao@users.noreply.github.com>
2023-08-11 17:23:21 +0400

    #14 | created functions create and read

--- db/script.py
+++ db/script.py
@@ -2,6 +2,37 @@ import sqlite3
 import time
 
 
+def create(name: str):
+    # while True:
+    con = sqlite3.connect('example_db.db')
+    cur = con.cursor()
+    # value = input("type: 'exit' to exit | 'create' to create a new entry \n")
+    # if value == 'exit':
+    #     break
+    # elif value == 'create':
+    date_time = time.strftime('%d.%m.%Y %H:%M:%S', time.localtime(time.time()))
+    # name = input('Enter the name: ')
+    cur.execute('INSERT INTO example_table (name, time) VALUES (?, ?)', (name, date_time))
+    con.commit()
+    con.close()
+
+
+def read(db, table: str):
+    con = sqlite3.connect(db)
+    cur = con.cursor()
+    for row in cur.execute(f'SELECT id, name, time FROM {table} ORDER BY time'):
+        print(row)
+    con.close()
+
+
+def update():
+    ...
+
+
+def delete():
+    ...
+
+
 def main():
     con = sqlite3.connect('example_db.db')
     cur = con.cursor()
@@ -13,20 +44,24 @@ def main():
     else:
         print("Table 'example_table' found!")
     # con.commit()
-    # con.close()
-
-    while True:
-        value = input("type: 'exit' to exit | 'create' to create a new entry \n")
-        if value == 'exit':
-            break
-        elif value == 'create':
-            # con = sqlite3.connect('example_db.db')
-            # cur = con.cursor()
-            date_time = time.strftime('%d.%m.%Y %H:%M:%S', time.localtime(time.time()))
-            name = input('Enter the name: ')
-            cur.execute('INSERT INTO example_table (name, time) VALUES (?, ?)', (name, date_time))
-            con.commit()
     con.close()
+
+    create('N')
+
+    read('example_db.db', 'example_table')
+
+    # while True:
+    #     value = input("type: 'exit' to exit | 'create' to create a new entry \n")
+    #     if value == 'exit':
+    #         break
+    #     elif value == 'create':
+    #         # con = sqlite3.connect('example_db.db')
+    #         # cur = con.cursor()
+    #         date_time = time.strftime('%d.%m.%Y %H:%M:%S', time.localtime(time.time()))
+    #         name = input('Enter the name: ')
+    #         cur.execute('INSERT INTO example_table (name, time) VALUES (?, ?)', (name, date_time))
+    #         con.commit()
+    # con.close()
 
 
 if __name__ == '__main__':
